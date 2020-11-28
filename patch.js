13e85baeaabd12a508af8dcc411acf20d5e273f1
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2020-11-28 15:10:44 +0700

    Create patch.js

---
+++ patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "THMBL_01",
+	"servers": [
+		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/THMBL_01/"
+	],
+	"title": "Touhou Mega Black Label 01 - UFO Arrange",
+	"update": true
+}
