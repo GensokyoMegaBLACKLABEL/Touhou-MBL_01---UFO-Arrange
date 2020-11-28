9bbd50cb6bfa66711b115a5c4848e10219c43760
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2020-11-28 14:50:45 +0700

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "YouTube: Gensokyo Mega BLACK LABEL",
+	"id": "Gensokyo Mega Black Label",
+	"patches": {
+		"THMBL_02": "Touhou Mega Black Label 01 - UFO Arrange"
+	},
+	"servers": [
+		"https://mirrors.thpatch.net//Touhou-MBL_01---UFO-Arrange"
+	],
+	"title": "Gensokyo Mega BLACK LABEL"
+}
